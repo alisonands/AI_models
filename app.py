288714445b6257b2f1df7d11f755e0c8042d34cf
alisonands <131694547+alisonands@users.noreply.github.com>
2025-02-26 14:40:08 -0500

    pls work

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ import anthropic #claude
 
 #for render
 import os
-gemini_api_key = os.getenv(gemini_api_key_render)
-openai_api_key = os.getenv(openai_api_key_render)
-claude_api_key = os.getenv(claude_api_key_render)
+gemini_api_key = os.getenv('gemini_api_key_render')
+openai_api_key = os.getenv('openai_api_key_render')
+claude_api_key = os.getenv('claude_api_key_render')
 
 # ---------GEMINI-----------
 # clients
