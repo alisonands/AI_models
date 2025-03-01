4401d4d78e85238c855824a1bb67cc7bd1bacd99
alisonands <131694547+alisonands@users.noreply.github.com>
2025-02-28 22:32:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import requests #llama
 import anthropic #claude
 
 #for render
-# import os
+import os
 gemini_api_key = os.getenv('gemini_api_key_render')
 openai_api_key = os.getenv('openai_api_key_render')
 claude_api_key = os.getenv('claude_api_key_render')
