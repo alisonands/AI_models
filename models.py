837134f0e301e641b506fa68ead423b82c1fbf52
alisonands <131694547+alisonands@users.noreply.github.com>
2025-03-04 23:41:15 -0500

    Update models.py

--- models.py
+++ models.py
@@ -96,7 +96,7 @@ def claude_chat(prompt):
         model = "claude-3-haiku-20240307",
         max_tokens=500,
         temperature=0,
-        system="You are a world class poet. reply in short messages only",
+        # system="You are a world class poet. reply in short messages only",
         messages=claude_conversation_history
     )
 
