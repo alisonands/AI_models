f87bfe92b1778a1b9d792a19daefa64ff9dc0028
alisonands <131694547+alisonands@users.noreply.github.com>
2025-03-06 23:41:09 -0500

    Update app.py

--- app.py
+++ app.py
@@ -147,7 +147,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("main.html")
 
 @app.route("/index.html")
 def main_page():
