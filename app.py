472e3b42206ed157b979ec6265cdfa72aa1b1440
alisonands <131694547+alisonands@users.noreply.github.com>
2025-03-23 12:02:31 -0400

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,12 @@ import anthropic #claude
 from sqlalchemy import create_engine, text
 from datetime import datetime
 
+# for pdf parsing
+import base64
+import pathlib
+from werkzeug.utils import secure_filename
+import os
+
 #for render
 import os
 gemini_api_key = os.getenv('gemini_api_key_render')
